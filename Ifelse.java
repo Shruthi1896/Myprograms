269439d5af51159eacea6d35b041c0ea7287f4e4
Shruthi1896 <60219230+Shruthi1896@users.noreply.github.com>
2020-01-29 17:16:11 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,17 @@
+class Ifelse{
+	public static void main(String args[]){
+		int number =20;
+				if (number%2==0){
+					System.out.println("even number");
+				}else{
+						System.out.println("odd number");
+						
+						
+						
+					}
+				}
+					
+					
+					
+		
+	}
